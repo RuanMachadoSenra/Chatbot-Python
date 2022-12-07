ca4c8e1a995a8c6dd06abb4bcf959ad353684ed0
Rodrigo <rcbm539@gmail.com>
2022-12-07 10:21:09 -0300

    02:01 - Expressões

---
+++ 01-chat_nltk.py
@@ -0,0 +1,16 @@
+from nltk.chat.util import Chat, reflections
+
+expressoes_pt = {
+    'eu': 'você',
+    'eu sou': 'você é',
+    'eu era': 'você era',
+    'eu iria': 'você iria',
+    'eu irei': 'você irá',
+    'meu': 'seu',
+    'você': 'eu',
+    'você é': 'eu sou',
+    'você era': 'eu era',
+    'você irá': 'eu irei',
+    'seu': 'meu',
+}
+
