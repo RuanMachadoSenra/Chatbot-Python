83bfb0f63673d8a49cc968c2aed45e865d32de80
Rodrigo <rcbm539@gmail.com>
2022-12-07 10:31:50 -0300

    04:04 - Similaridade entre Palavras

--- 03-pergunta_resposta.py
+++ 03-pergunta_resposta.py
@@ -27,6 +27,13 @@ texto_principal.append(texto_principal[0])
 
 vetor_palavras = TfidfVectorizer()
 palavras_rotuladas = vetor_palavras.fit_transform(texto_principal)
-print(palavras_rotuladas)
-print(vetor_palavras.get_feature_names()) # palavras únicas
-print(vetor_palavras.vocabulary_) # id das palavras
+#print(palavras_rotuladas)
+#print(vetor_palavras.get_feature_names()) # palavras únicas
+#print(vetor_palavras.vocabulary_) # id das palavras
+
+similaridade = cosine_similarity(palavras_rotuladas[0], palavras_rotuladas[2])
+similaridade_todos = cosine_similarity(palavras_rotuladas[0], palavras_rotuladas)
+print(similaridade_todos)
+print(similaridade_todos.argsort())
+x = similaridade_todos.argsort()[0]
+print(x[-2])
