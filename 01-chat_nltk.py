59bf8bafd06d181bb12efba27c7444aa8ade68c5
Rodrigo <rcbm539@gmail.com>
2022-12-07 10:22:23 -0300

    02:02 - Sentenças

--- 01-chat_nltk.py
+++ 01-chat_nltk.py
@@ -14,3 +14,33 @@ expressoes_pt = {
     'seu': 'meu',
 }
 
+sentencas = [
+    [
+       r'oi|olá|opa|eae',
+        ['olá', 'como vai?', 'tudo bem?']
+    ],
+    [
+        r'qual é o seu nome?',
+        ['Meu nome é TwBot.  Em que posso ajudá-lo?']
+    ],
+    [
+        r'(.*) sua idade',
+        ['Não tenho idade pois sou um chatbot']
+    ],
+    [
+        r'meu nome é (.*)',
+        ['Olá %1, como você está hoje?']
+    ],
+    [
+        r'qual formação você indica em Python?',
+        ['Formação Flask', 'Formação Django', 'Formação ML com Python']
+    ],
+    [
+        r'quais tecnologias estão em alta no mercado?',
+        ['Flutter', 'React', 'NextJS']
+    ],
+    [
+        r'quit',
+        ['Até breve', 'Foi bom conversar com você.']
+    ]
+]
