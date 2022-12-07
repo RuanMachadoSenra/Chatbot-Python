ed5d3613d73f56fc166d328eff630db76cc79d86
Rodrigo <rcbm539@gmail.com>
2022-12-07 10:25:46 -0300

    03:02 - Treinamento do Modelo

--- 02-chatterbot.py
+++ 02-chatterbot.py
@@ -5,3 +5,28 @@ from spacy.cli import download
 
 download("en_core_web_sm")
 
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+from spacy.cli import download
+
+#download("en_core_web_sm")
+
+class ENGSM:
+    ISO_639_1 = 'en_core_web_sm'
+
+chatbot = ChatBot("BotTw", tagger_language=ENGSM)
+
+sentenca = [
+    "opa",
+    "como vai?",
+    "Qual seu nome?",
+    "Sou o BotTw e vou te ajudar na carreira de tecnologia",
+    "Qual formação você indica em python?",
+    "Recomendo a Formação Flask",
+    "Qual tecnologia está em alta no momento?",
+    "Flutter"
+]
+
+treino = ListTrainer(chatbot)
+treino.train(sentenca)
